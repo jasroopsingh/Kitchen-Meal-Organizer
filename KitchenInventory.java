e9b4e55287ced2228a1301b00b8d19561aa9eccc
Jasroop Singh <jasroop.tech@gmail.com>
2024-05-25 11:16:18 -0500

    t

--- KitchenInventory.java
+++ KitchenInventory.java
@@ -1,23 +1,19 @@
+import java.util.List;
+
 public class KitchenInventory 
 {
     //contain functions to sort by name/quantity
     //compare for req ingredients
 
     // Adds an ingredient to the inventory
-    public void addIngredient() {}
+    public void addIngredient() { }
 
     // Removes a specified quantity of an ingredient from the inventory
-    public void removeIngredient() {}
+    public void removeIngredient() { }
 
     // Sorts ingredients by name and displays them
-    public void sortByName() 
-    {
-        Ingredient[] NameList;
-        NameList = new Ingredient[5];
+    public void sortByName() { }
 
-
-
-    }
 
     // Sorts ingredients by quantity and displays them
     public void sortByQuantity() 
@@ -41,7 +37,8 @@ public class KitchenInventory
     }
 
     // Displays a list of ingredients
-    public void displayIngredients() {}
+    public void displayIngredients(List<Ingredient> ingredientList) 
+    { for (Ingredient ingredient : ingredientList)  { System.out.println(ingredient);} } // Print ingredient details
 
     // Compares the inventory with the ingredients required for a meal
     public void compareIngredients(Meal meal) {}
--- Meal.java
+++ Meal.java
@@ -1,9 +1,24 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Meal
 {
+    private String name; // Name of the meal
+    private List<Ingredient> requiredIngredients; // List to store required ingredients
 
+    public Meal(String name) 
+    {
+        this.name = name; // Initialize meal name
+        this.requiredIngredients = new ArrayList<>(); // Initialize ingredients list
+    }
+
+    // Returns the name of the meal
+    public String getName() { return name; }
+
+    // Adds an ingredient to the meal's required ingredients
+    public void addIngredient(Ingredient ingredient) { requiredIngredients.add(ingredient); } // Add ingredient to list
     
-// use maps/hashmaps to map meals to their req ingredents
-// https://www.w3schools.com/java/java_hashmap.asp
+
+    // Returns the list of required ingredients
+    public List<Ingredient> getRequiredIngredients() { return requiredIngredients; }
 }
